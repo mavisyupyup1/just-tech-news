e1f2decf69d6347793db1b712eb5c6f929265de5
Your Name <graceliu@gracelius-iMac.home>
2021-12-22 16:12:40 -0500

    create posts model and routes

--- models/Post.js
+++ models/Post.js
@@ -1,43 +1,42 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {}
 
-//create out Post model
-class Post extends Model{};
-// define columns in post and pass current connection instance to initialize the Post model
+// create fields/columns for Post model
 Post.init(
-    //define Post schema
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey:true,
-            autoIncrement:true
-        },
-        title:{
-            type:DataTypes.STRING,
-            allowNull: false
-        },
-        post_url:{
-            type:DataTypes.STRING,
-            allowNull:false,
-            validate:{
-                isURL:true
-            }
-        },
-        user_id:{
-            type:DataTypes.STRING,
-            references:{
-                model:'user',
-                key:'id'
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    //configure metadata
-    {
-        sequelize,
-        freezeTableName:true,
-        underscored:true,
-        modelName:'post'
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
 );
-module.exports =Post;
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
-const User =require('./User');
-const Post = require('./Post')
-module.exports ={User, Post};
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+//reverse associations
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
