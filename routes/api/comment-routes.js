1bbc822fdf2452a1446165817e552fb9b2e65228
mavisyupyup1 <mavisyupyup1@github.com>
2021-12-27 12:26:59 -0500

    added delete function

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -3,7 +3,7 @@ const {Comment} = require('../../models');
 
 router.get('/',(req,res)=>{
     Comment.findAll({
-        attributes:['id',' comment_text','user_id','post_id']
+        attributes:['id','comment_text','user_id','post_id']
     })
     .then(dbCommentData=>res.json(dbCommentData))
     .catch(err=>{
@@ -26,7 +26,22 @@ router.post('/',(req,res)=>{
 })
 
 router.delete('/:id',(req,res)=>{
-
+    Comment.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then(dbCommentData=>{
+        if(!dbCommentData){
+            res.status(404).json({message:'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData)
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err)
+    })
 })
 
 module.exports = router;
